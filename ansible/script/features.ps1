c6b271e7ff2608905a50407e268791f84e5c5d56
bertilmolele <91033311+bertilmolele@users.noreply.github.com>
2021-10-28 16:32:38 +0200

    Update features.ps1

--- ansible/script/features.ps1
+++ ansible/script/features.ps1
@@ -1,4 +1,3 @@
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebserverRole -NoRestart
 python -m pip install --upgrade pip
 python -m pip install -r C:\cardabel_dir\python\requirements.txt
-Import-Csv C:\cardabel\install\scripts\Roles.csv | foreach { Install-WindowsFeature $_name}
