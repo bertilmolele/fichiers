99129715dd54f03b5e633a64995dbe605e929c88
bertilmolele <91033311+bertilmolele@users.noreply.github.com>
2021-12-03 15:41:00 +0100

    Delete ansible directory

--- ansible/script/db-config.ps1
+++
@@ -1,7 +0,0 @@
-$new_db = 'yes'
-$db_migration_file = 'C:\sources\anomaly_ocbc.dump'
-if ($new_db -eq 'yes') {
-    if (-not $db_migration_file) {
-	Start-Process "python" -NoNewWindow -ArgumentList "C:\cardabel\cardabel-config\create_db.py cardabel cardabel$ 34.107.64.214 3386 bertil2 C:\cardabel\install" -Wait}  
-
-}
--- ansible/script/features.ps1
+++
@@ -1,3 +0,0 @@
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebserverRole -NoRestart
-python -m pip install --upgrade pip
-python -m pip install -r C:\cardabel_dir\python\requirements.txt
